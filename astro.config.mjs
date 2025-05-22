40eaaa5c49fb0cf0c81d5ea0133d17129dd73693
xujiuran <chaojie.xu@visionacademy.cn>
2025-05-22 15:15:29 +0800

    fix: :bug: updat base ang site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xujiuran.github.io/tomato.astro5',
-  base: '/',
+  site: 'https://xujiuran.github.io/',
+  base: 'tomato.astro5/',
   integrations: [preact()]
 });
